5369e2021098f98a8f2f964276104caa8e6db034
Miguel <multimedia.publicidad@gmail.com>
2017-11-30 22:45:10 -0600

    Genesis
    Base Movistar

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('#slider').slick({
+		dots: true,
+		dotsClass: "slider-circles",
+		arrows: false
+  });
+  $('#slider-tienda').slick({
+		dots: true,
+		arrows: false,
+		autoplay: true
+  });
+});
+	
